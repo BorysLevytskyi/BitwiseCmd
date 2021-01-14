72adb7a5b3b30fc9a0010bbb8575b6d67cdd0240
Borys_Levytskyi <Borys_Levytskyi@epam.com>
2021-01-14 22:53:52 +0200

    Release with better icons

---
+++ precache-manifest.41bb06c770f194311124a6204464b04c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45dab1ce82c60cd3c8f752c4e456407c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fea57061d29572ac79ed",
+    "url": "/static/css/main.263615b1.chunk.css"
+  },
+  {
+    "revision": "f102156b722bb7c1916b",
+    "url": "/static/js/2.59417331.chunk.js"
+  },
+  {
+    "revision": "fea57061d29572ac79ed",
+    "url": "/static/js/main.622412c9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.de67bb30f30431eb5e9648c231c31f5a.js"
+  "/precache-manifest.41bb06c770f194311124a6204464b04c.js"
 );
 
 self.addEventListener('message', (event) => {
