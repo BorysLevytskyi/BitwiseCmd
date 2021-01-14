b72c43050d552d313d101cd1ba0b369f5c1f0ea5
Borys_Levytskyi <Borys_Levytskyi@epam.com>
2021-01-14 10:52:35 +0200

    Fix release

---
+++ precache-manifest.9b9c2f78f5a59925837e68e7b3e2ab6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a41ca4b37e9adea51a020f4a8bbf2d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbaaae3a758ac2e79a69",
+    "url": "/static/css/main.65e4658e.chunk.css"
+  },
+  {
+    "revision": "350cc709b19ae89abcc6",
+    "url": "/static/js/2.d12d1bb6.chunk.js"
+  },
+  {
+    "revision": "fbaaae3a758ac2e79a69",
+    "url": "/static/js/main.373fb199.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
