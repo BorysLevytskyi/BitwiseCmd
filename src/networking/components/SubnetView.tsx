4c9d0c61252776fb624bda8c49d3d425176a0152
Borys_Levytskyi <Borys_Levytskyi@epam.com>
2021-01-19 19:08:39 +0200

    Implement ability to increase/decrease subnet mask length

--- src/networking/components/SubnetView.tsx
+++ src/networking/components/SubnetView.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BinaryStringView from '../../core/components/BinaryString';
 import './SubnetView.css';
 import { getNetworkAddress, getBroadCastAddress, createSubnetMaskIp } from '../subnet-utils';
 import { chunkifyString } from '../../core/utils';
 import IpAddressBinaryString from './IpAddressBinaryString';
-import { IpAddress, SubnetCommand } from '../models';
+import { IpAddress, IpAddressWithSubnetMask, SubnetCommand } from '../models';
 
-function SubnetView({subnet} : {subnet : SubnetCommand}) {
+function SubnetView(props : {subnet : SubnetCommand}) {
+
+    const [subnet, setSubnet] = useState(props.subnet);
+
+    const incrementMask = () => {
+        const newInput = new IpAddressWithSubnetMask(subnet.input.ipAddress, subnet.input.maskBits+1);
+        setSubnet(new SubnetCommand(newInput));
+    };
+
+    const decrementMask = () => {
+        const newInput = new IpAddressWithSubnetMask(subnet.input.ipAddress, subnet.input.maskBits-1);
+        setSubnet(new SubnetCommand(newInput));
+    }
 
     return <React.Fragment>
         <table className="expression subnet-view">
@@ -16,11 +28,22 @@ function SubnetView({subnet} : {subnet : SubnetCommand}) {
                     <SubnetRow ip={createSubnetMaskIp(subnet.input)} descr="Net Mask"/>
                     <SubnetRow ip={getBroadCastAddress(subnet.input)} descr="Broadcast"/>                  
                     <tr>
-                        <td className="description soft">
+                        <td data-test-name="label" className="soft">
                             <span>Network Size</span>
                         </td>
-                        <td>
+                        <td data-test-name="decimal">
                             {subnet.getAdressSpaceSize()}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td data-test-name="label" className="soft">
+                            Mask Size
+                        </td>
+                        <td data-test-name="decimal">
+                           
+                            <button onClick={decrementMask} disabled={subnet.input.maskBits === 0} title="Decrease mask size">-</button>
+                            <span>{subnet.input.maskBits}</span>
+                            <button onClick={incrementMask} disabled={subnet.input.maskBits === 32} title="Increase mask size">+</button>
                         </td>
                     </tr>
             </tbody>
@@ -35,18 +58,14 @@ function SubnetRow(props: { ip: IpAddress, descr: string}) {
     const {ip, descr} = props;
 
     return <tr>
-            <td className="description soft">{descr}</td>
-                <td className="ip">
+            <td className="soft" data-test-name="label">{descr}</td>
+                <td data-test-name="decimal" className="ip-address-col">
                    {ip.toString()}
                 </td>
-                <td className="class-part">
+                <td data-test-name="bin">
                     <IpAddressBinaryString ip={ip} />
             </td>
         </tr>;
-
-    function addDots(bin: string) {
-        return chunkifyString(bin, 8).map((s, i) => <BinaryStringView binaryString={s} key={i} />)
-    }
 }
 
 export default SubnetView;
