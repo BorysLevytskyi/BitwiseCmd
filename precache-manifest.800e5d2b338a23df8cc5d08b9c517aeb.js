9a8f09c4a718a7f97a9efd9ecd4e40357585359a
Borys_Levytskyi <Borys_Levytskyi@epam.com>
2021-02-19 18:42:44 +0200

    Release with BTC donations

---
+++ precache-manifest.800e5d2b338a23df8cc5d08b9c517aeb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00a52adf14f13dc0af854c08106fda73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13323716cdb2e6077c67",
+    "url": "/static/css/main.a6a13122.chunk.css"
+  },
+  {
+    "revision": "5903b87e33572f5239d7",
+    "url": "/static/js/2.6536828e.chunk.js"
+  },
+  {
+    "revision": "13323716cdb2e6077c67",
+    "url": "/static/js/main.79ede039.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.71e0c98216dd43baf910a39f4f3bd5ae.js"
+  "/precache-manifest.800e5d2b338a23df8cc5d08b9c517aeb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
