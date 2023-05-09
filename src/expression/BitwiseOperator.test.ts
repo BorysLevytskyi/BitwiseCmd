527caeb7fd9a8a84fd2e2c9cb1c3f20e42fdd709
BorysLevytskyi <borys.levytskyi@gmail.com>
2023-05-09 13:37:38 +0200

    commit

--- src/expression/BitwiseOperator.test.ts
+++ src/expression/BitwiseOperator.test.ts
@@ -18,29 +18,3 @@ it('can apply & operand', () => {
     expect(result.value.num()).toBe(7);
     expect(result.base).toBe('dec');
 });
-
-it('treats differently big ints and regular numbers', () => {
-    const one = new BoundedInt(1);
-    const n64 = new BoundedInt(2147483647, 64);
-    const n32 = new BoundedInt(2147483647, 32);
-
-    const bigResult = engine.applyOperator(new ScalarValue(n64), "<<", new ScalarValue(one));
-    const result = engine.applyOperator(new ScalarValue(n32), "<<", new ScalarValue(one));
-
-    expect(bigResult.value.toString()).toBe("4294967294");
-    expect(result.value.toString()).toBe("-2");
-});
-
-it("can execute all operators without errors", () => {
-
-    const operators = [">>", "<<", "|", "&", "^"];
-
-    // >>> not supported by BigInt
-    expect(() => engine.applyOperator(new ScalarValue(1),  ">>>", new ScalarValue(2))).not.toThrow();
-
-    operators.forEach(o => {
-        expect(() => engine.applyOperator(new ScalarValue(BigInt(1)), o, new ScalarValue(BigInt(2)))).not.toThrow();
-        expect(() => engine.applyOperator(new ScalarValue(1), o, new ScalarValue(2))).not.toThrow();
-    });
-    
-});
