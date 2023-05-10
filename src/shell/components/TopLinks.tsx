446282d797e1c1f94ddb95ee940a0aa49ea0bd3e
BorysLevytskyi <borys.levytskyi@gmail.com>
2023-05-10 15:22:56 +0200

    commit

--- src/shell/components/TopLinks.tsx
+++ src/shell/components/TopLinks.tsx
@@ -10,7 +10,7 @@ function TopLinks() {
     return <ul className="top-links">                
                 <li>
                     <a href='https://www.paypal.com/donate/?hosted_button_id=3GREJYC4T5AJ8' onClick={onDonate} target="_blank">
-                        <FontAwesomeIcon className='icon' icon={faDonate} size="lg" />donate
+                        <FontAwesomeIcon className='icon' icon={faDonate} size="lg" /><span className='link-text'>donate</span>
                     </a>
                 </li>
                 <li>
